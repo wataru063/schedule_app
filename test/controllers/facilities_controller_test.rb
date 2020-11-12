3763e0dc318f658822ec525c925bfa2ada601ca5
wataru <skgch0702@gmail.com>
2020-11-12 17:05:10 +0900

    Remove unnecessary files

--- test/controllers/facilities_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-class FacilitiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get facility_path
-    assert_response :success
-    assert_select "title", "荷役設備登録 | 需給管理システム"
-  end
-end
--- test/controllers/users_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-    assert_select "title", "新規登録 | 需給管理システム"
-  end
-end
